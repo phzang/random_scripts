2205faa029d9c9cf4c41e4ac6471142bbd0777a2
P Z <40149803+phzang@users.noreply.github.com>
2023-03-13 20:36:21 -0400

    Create debian_install_script.sh

---
+++ debian_install_script.sh
@@ -0,0 +1,25 @@
+# Common programs to install after a fresh install
+
+
+#bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y htop curl build-essential python3 pip git gdb 
+# Install Docker
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+# Download Syncthing
+# Add the release PGP keys:
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+# Add the "stable" channel to your APT sources:
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt-get update
+sudo apt-get install -y syncthing
